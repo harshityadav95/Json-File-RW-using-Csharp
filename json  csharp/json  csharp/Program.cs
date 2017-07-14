73fbd611ee25b11112eefc94831a22b465c300cf
Harshit Yadav <harshityadav95@gmail.com>
2017-07-15 05:01:12 +0530

    New File not created

--- json  csharp/json  csharp/Program.cs
+++ json  csharp/json  csharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,15 +9,23 @@ using System.Web.Script.Serialization;
 
 namespace json__csharp
 {
-    class person
+    class item  :IEquatable<item>
     {
-        public string name { get; set; }
-        public int age { get; set; }
-        public override string ToString()
+        public string name;
+        public int price;
+
+        public item(string name, int price = 0)
         {
-            return string.Format("Name  :  {0} \n Age {1}", name, age);
+
+            this.name = name;
+            this.price = price;
         }
 
+        public bool Equals(item other)
+        {
+            if (other == null) return false;
+            return (this.name.Equals(other.name));
+        }
     }
 
     class Program
@@ -26,19 +35,93 @@ namespace json__csharp
         {
             // Deserializing the JSON File  
             Console.WriteLine("hELLO WORLD  ");
+            string v = "x";
+            string b = "y";
+            string c = v + b;
+            Console.WriteLine(c);
 
-            ////for reading the json file
-            String jsonString = File.ReadAllText("JSON.json");
 
-            JavaScriptSerializer ser = new JavaScriptSerializer();
-            person p1 = ser.Deserialize<person>(jsonString);
-            Console.WriteLine(p1);
+            // creating json file if path does not exit already  
 
-            // for wrtiting the json file  
+            string path = Directory.GetCurrentDirectory() + "data.json";
+           
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+               
+                
+            }
 
-            person p2 = new person() { name = "Harddddshit", age = 22 };
-            string outputjson = ser.Serialize(p2);
-            File.WriteAllText("JSON.json", outputjson);
+
+            // read the files into a string and deserialize JSON to a type  
+            Console.WriteLine("Reading Data .json");
+            string jsonString = File.ReadAllText("data.json");
+            List<item> myList = JsonConvert.DeserializeObject<List<item>>(jsonString);
+
+            if (myList == null)
+                myList = new List<item>();
+            // if list is empty we need to add item to the list since it cannot be blank  
+
+
+
+            string input = "";
+            int inputInt = 0;
+            string inputString = "";
+
+            while (input != "q")
+            {
+                Console.WriteLine("Enter A to add new Item ");
+                Console.WriteLine("Enter D to delete Item ");
+                Console.WriteLine("Enter S to Show content ");
+                Console.WriteLine("Enter Q to exit the program ");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "a":
+                        Console.WriteLine("Adding new Item ");
+                        Console.WriteLine("Enter Item name ");
+                        inputString = Console.ReadLine();
+                        Console.WriteLine("Enter the Price Value in digits only");
+                        inputInt = Convert.ToInt32(Console.ReadLine());
+                        myList.Add(new item(inputString, inputInt));
+                        Console.WriteLine("Added Item " + inputString + " with price " + inputInt);
+                        break;
+                    case "d":
+                        Console.WriteLine("Deleting an Item ");
+                        Console.WriteLine("Enter the Item to be Deleted ");
+                        inputString = Console.ReadLine();
+                        myList.Remove(new item(inputString));
+                        Console.WriteLine("Deleted item with name " + inputString);
+
+                        break;
+
+
+                    case "s":
+                        Console.WriteLine("\n Showing contents");
+                        foreach (item a in myList)
+                        {
+                            Console.WriteLine("Item : " + a.name + " and Price " + a.price);
+
+                        }
+                        Console.WriteLine("\n");
+
+                       break;
+
+                    default:
+                        Console.WriteLine("Incorrect Command , try Again");
+
+                        break;
+
+
+                }
+                Console.WriteLine("Rewriting  data.json");
+                string data = JsonConvert.SerializeObject(myList);
+                File.WriteAllText("data.json",data);
+
+                Console.ReadKey();
+
+            }
+
 
 
             Console.ReadLine();
